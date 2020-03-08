5bddfda737ebe48a446a737e34aef86302cdc6b4
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-03-09 08:36:13 +0900

    solve test_try_over Q1, Q2

--- 03_method/try_over3_3.rb
+++ 03_method/try_over3_3.rb
@@ -5,7 +5,17 @@ TryOver3 = Module.new
 # - `test_` から始まるインスタンスメソッドが実行された場合、このクラスは `run_test` メソッドを実行する
 # - `test_` メソッドがこのクラスに実装されていなくても `test_` から始まるメッセージに応答することができる
 # - TryOver3::A1 には `test_` から始まるインスタンスメソッドが定義されていない
+class TryOver3::A1
+  def run_test
+    nil
+  end
 
+  def method_missing(name, *args)
+    return run_test if name.to_s.start_with?("test_")
+
+    super
+  end
+end
 
 # Q2
 # 以下要件を満たす TryOver3::A2Proxy クラスを作成してください。
@@ -18,6 +28,19 @@ class TryOver3::A2
   end
 end
 
+class TryOver3::A2Proxy
+  def initialize(source)
+    @source = source
+  end
+
+  def respond_to?(name)
+    @source.respond_to?(name) || super
+  end
+
+  def method_missing(name, *args)
+    @source.send(name.to_sym, *args)
+  end
+end
 
 # Q3
 # 前回 OriginalAccessor の my_attr_accessor で定義した getter/setter に boolean の値が入っている場合には #{name}? が定義されるようなモジュールを実装しました。
